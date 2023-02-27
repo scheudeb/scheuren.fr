f00a1129d6f306922a57ae77621ca880862f8397
Déborah Scheuren <110463007+scheudeb@users.noreply.github.com>
2023-02-27 17:35:00 +0100

    modifications bugs

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
+//Gestion du mode Dark
 document.addEventListener('DOMContentLoaded', () => {
+    const body = document.body;
     const btnToggle = document.querySelector('.btn_toggle');
-    btnToggle.addEventListener('click', () => {
-        const body = document.body;
 
+    if (body.classList.contains('dark')) {
+        btnToggle.innerHTML = '<i class="fa-solid fa-sun"></i>';
+    } else if (body.classList.contains('light')) {
+        btnToggle.innerHTML = '<i class= "fa-solid fa-moon"></i>';
+    }
+
+    btnToggle.addEventListener('click', () => {
         if (body.classList.contains('dark')) {
             body.classList.add('light')
             body.classList.remove('dark')
@@ -14,6 +21,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
 document.addEventListener("DOMContentLoaded", function () {
     const slider = document.querySelector('.slideshow_container');
     let holding = false;
